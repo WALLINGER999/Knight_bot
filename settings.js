6c136ad53784c53fe1c5deef3e91141eba993d38
WALLINGER999 <151752800+WALLINGER999@users.noreply.github.com>
2025-04-21 17:53:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'walling',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Walling's Bot",
+  botOwner: 'walling', // Your name
+  ownerNumber: '918798437489', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
